b7aab022ae9f643290a6f72eaabc20a73a6eb986
jrdouceur <johndo@microsoft.com>
2022-12-08 08:17:13 -0800

    add mock parser and release action (#3)

---
+++ mock/Parser/ModelParser.cs
@@ -0,0 +1,13 @@
+// <copyright file="ModelParser.cs" company="Digital Twin Consortium">
+// Copyright (c) Digital Twin Consortium. All rights reserved.
+// </copyright>
+
+namespace DTDLParserMock
+{
+    public class ModelParser
+    {
+        public ModelParser()
+        {
+        }
+    }
+}
