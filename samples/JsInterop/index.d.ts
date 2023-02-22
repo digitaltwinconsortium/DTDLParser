431ee661b1aba917eef99a9ca1eae096e0990979
Auto Gen <johndo@microsoft.com>
2023-02-22 10:29:28 -0800

    fix .d.ts refs in index.d.ts

--- samples/JsInterop/index.d.ts
+++ samples/JsInterop/index.d.ts
@@ -1,7 +1,7 @@
 
 import { DtdlObjectModel } from "./generated/DtdlOm";
-export * from "./generated/DtdlOm";
-export * from "./DtdlErr"
+export * from "../../javascript/generated/DtdlOm";
+export * from "../../javascript/DtdlErr"
 
 declare module "dtdl-parser-interop" {
     export function parseAsync(jsonTexts: string | string[]): Promise<DtdlObjectModel>
