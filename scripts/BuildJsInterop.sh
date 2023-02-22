4e705caee244b247598d2b5949e4ff45884a8ff0
Auto Gen <johndo@microsoft.com>
2023-02-22 05:32:22 -0800

    add dotnet workload restore

--- scripts/BuildJsInterop.sh
+++ scripts/BuildJsInterop.sh
@@ -8,6 +8,8 @@ fi
 
 bash scripts/Title.sh BuildJsInterop $DtdlModelParserBuildConfig
 
+dotnet workload restore --project javascript\JsInterop.csproj
+
 dotnet publish javascript --configuration $DtdlModelParserBuildConfig
 
 test $? -eq 0 || bash scripts/Failure.sh BuildJsInterop $DtdlModelParserBuildConfig
